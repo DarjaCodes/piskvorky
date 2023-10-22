367f81f970bad1fcb25b3d280bb1274d312124d0
DarjaCodes <krjukova4darja@gmail.com>
2023-10-22 18:15:49 +0200

    final

--- hra.js
+++ hra.js
@@ -2,54 +2,52 @@ import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
 
 let currentPlayer = 'circle';
 const buttonsClick = document.querySelectorAll('.box');
+const playerIndicator = document.querySelector('#player-indicator');
 
-/* Prvni funkce pro zobrazeni symbolu*/
+/* Prvni funkce pro zobrazeni symbolu a API*/
 const selectLogo = async (event) => {
   if (currentPlayer === 'circle') {
-    event.target.classList.contains('board__field--cross');
-    event.target.classList.add('board__field--circle');
-    document.querySelector('#player-indicator').src = 'images/cross2.svg';
+    event.target.classList.toggle('board__field--circle');
+    playerIndicator.src = 'images/cross2.svg';
     currentPlayer = 'cross';
     event.target.disabled = true;
-
-    // Kontrola, zda je na tahu krizek
-    if (currentPlayer === 'cross') {
-      const buttonsArray = Array.from(buttonsClick).map((field) => {
-        if (field.classList.contains('board__field--cross')) {
-          return 'x';
-        } else if (field.classList.contains('board__field--circle')) {
-          return 'o';
-        } else {
-          return '_';
-        }
-      });
-
-      const response = await fetch(
-        'https://piskvorky.czechitas-podklady.cz/api/suggest-next-move',
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            board: buttonsArray,
-            player: 'x',
-          }),
-        },
-      );
-
-      const data = await response.json();
-      const { x, y } = data.position;
-      const field = buttonsClick[x + y * 10];
-      field.click();
-    }
-  }
-  if (currentPlayer === 'cross') {
-    event.target.classList.contains('board__field--circle');
-    event.target.classList.add('board__field--cross');
-    document.querySelector('#player-indicator').src = 'images/circle2.svg';
+  } else if (currentPlayer === 'cross') {
+    event.target.classList.toggle('board__field--cross');
+    playerIndicator.src = 'images/circle2.svg';
     currentPlayer = 'circle';
     event.target.disabled = true;
+  }
+
+  if (currentPlayer === 'cross') {
+    const buttonsArray = Array.from(buttonsClick).map((field) => {
+      if (field.classList.contains('board__field--cross')) {
+        return 'x';
+      } else if (field.classList.contains('board__field--circle')) {
+        return 'o';
+      } else {
+        return '_';
+      }
+    });
+
+    const response = await fetch(
+      'https://piskvorky.czechitas-podklady.cz/api/suggest-next-move',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          board: buttonsArray,
+          player: 'x',
+        }),
+      },
+    );
+
+    const data = await response.json();
+    const { x, y } = data.position;
+    const field = buttonsClick[x + y * 10];
+    console.log(data);
+    field.click();
   }
 };
 
