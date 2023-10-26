7b86508870f5dade915cf00b9b83646bc47fa2da
DarjaCodes <krjukova4darja@gmail.com>
2023-10-26 18:45:45 +0200

    sjednoceni funkci

--- hra.js
+++ hra.js
@@ -4,31 +4,42 @@ let currentPlayer = 'circle';
 const buttonsClick = document.querySelectorAll('.box');
 const playerIndicator = document.querySelector('#player-indicator');
 
-/* Prvni funkce pro zobrazeni symbolu a API*/
-const selectLogo = async (event) => {
+// Obecná funkce pro zpracování tahu a kontrolu výsledku, API
+const handleMove = async (event) => {
   if (currentPlayer === 'circle') {
     event.target.classList.toggle('board__field--circle');
     playerIndicator.src = 'images/cross2.svg';
     currentPlayer = 'cross';
-    event.target.disabled = true;
   } else if (currentPlayer === 'cross') {
     event.target.classList.toggle('board__field--cross');
     playerIndicator.src = 'images/circle2.svg';
     currentPlayer = 'circle';
-    event.target.disabled = true;
+  }
+  event.target.disabled = true;
+
+  const buttonsArray = Array.from(buttonsClick).map((field) => {
+    if (field.classList.contains('board__field--cross')) {
+      return 'x';
+    } else if (field.classList.contains('board__field--circle')) {
+      return 'o';
+    } else {
+      return '_';
+    }
+  });
+
+  const winner = findWinner(buttonsArray);
+  if (winner === 'x' || winner === 'o' || winner === 'tie') {
+    setTimeout(() => {
+      if (winner === 'tie') {
+        alert('Hra skoncila remízou');
+      } else {
+        alert(`Vyhrál hráč ${winner.toUpperCase()}`);
+      }
+      location.reload();
+    }, 1000);
   }
 
   if (currentPlayer === 'cross') {
-    const buttonsArray = Array.from(buttonsClick).map((field) => {
-      if (field.classList.contains('board__field--cross')) {
-        return 'x';
-      } else if (field.classList.contains('board__field--circle')) {
-        return 'o';
-      } else {
-        return '_';
-      }
-    });
-
     const response = await fetch(
       'https://piskvorky.czechitas-podklady.cz/api/suggest-next-move',
       {
@@ -46,49 +57,16 @@ const selectLogo = async (event) => {
     const data = await response.json();
     const { x, y } = data.position;
     const field = buttonsClick[x + y * 10];
-    console.log(data);
     field.click();
   }
 };
 
-/* Vytvoreni pole a druha funkce*/
-const checkBoard = () => {
-  const buttonsArray = Array.from(buttonsClick).map((field) => {
-    if (field.classList.contains('board__field--cross')) {
-      return 'x';
-    } else if (field.classList.contains('board__field--circle')) {
-      return 'o';
-    } else {
-      return '_';
-    }
-  });
-  const winner = findWinner(buttonsArray);
-  if (winner === 'x') {
-    setTimeout(() => {
-      alert('Vyhrál hráč X');
-      location.reload();
-    }, 1000);
-  } else if (winner === 'o') {
-    setTimeout(() => {
-      alert('Vyhrál hráč O');
-      location.reload();
-    }, 1000);
-  } else if (winner === 'tie') {
-    setTimeout(() => {
-      alert('Hra skoncila remízou');
-      location.reload();
-    }, 1000);
-  }
-};
-/* Posluchace udalosti*/
+// Posluchace události a zavolání funkce
 document.querySelectorAll('.box').forEach((button) => {
-  button.addEventListener('click', function () {
-    selectLogo(event);
-    checkBoard();
-  });
+  button.addEventListener('click', handleMove);
 });
 
-/* Osetreni restartu*/
+// Ošetření restartu
 document
   .querySelector('.menu__buttons--restart')
   .addEventListener('click', (event) => {
